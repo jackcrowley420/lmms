4cb0e8cb95f077566198ff20a9397c2fb318cce2
jackcrowley420 <187197904+jackcrowley420@users.noreply.github.com>
2026-04-02 13:55:40 -0400

    Revert "Squashed commit of the following:"
    This reverts commit 4bfdb37f14bb8d0c7722295d3213791494e7060a.

--- plugins/CarlaBase/Carla.cpp
+++ plugins/CarlaBase/Carla.cpp
@@ -162,9 +162,6 @@ CarlaInstrument::CarlaInstrument(InstrumentTrack* const instrumentTrack, const D
     fHost.uiParentId  = 0;
 
     // carla/resources contains PyQt scripts required for launch
-#if defined(LMMS_CARLA_RESOURCES_PATH)
-    QString resourcesPath = QStringLiteral(LMMS_CARLA_RESOURCES_PATH);
-#else
     QDir path(carla_get_library_folder());
 #if defined(CARLA_OS_LINUX)
     path.cdUp();
@@ -173,7 +170,6 @@ CarlaInstrument::CarlaInstrument(InstrumentTrack* const instrumentTrack, const D
 #else
     // parse prefix from dll filename
     QString resourcesPath = path.absolutePath() + "/resources";
-#endif
 #endif
     fHost.resourceDir            = strdup(resourcesPath.toUtf8().constData());
     fHost.get_buffer_size        = host_get_buffer_size;
@@ -882,11 +878,20 @@ CarlaParamsView::~CarlaParamsView()
 	// Close and delete m_paramsSubWindow
 	if (m_carlaInstrumentView->m_paramsSubWindow)
 	{
+		m_carlaInstrumentView->m_paramsSubWindow->setAttribute(Qt::WA_DeleteOnClose);
 		m_carlaInstrumentView->m_paramsSubWindow->close();
+
+		delete m_carlaInstrumentView->m_paramsSubWindow;
 		m_carlaInstrumentView->m_paramsSubWindow = nullptr;
 	}
 
 	m_carlaInstrumentView->m_paramsView = nullptr;
+
+	// Clear models
+	if (!m_carlaInstrument->m_paramModels.empty())
+	{
+		m_carlaInstrument->clearParamModels();
+	}
 }
 
 void CarlaParamsView::clearFilterText()
@@ -974,9 +979,10 @@ void CarlaParamsView::refreshKnobs()
 
 	// Clear max knob width per group
 	m_maxKnobWidthPerGroup.clear();
-	for (uint8_t i = 0; i < m_carlaInstrument->m_paramGroupCount; ++i)
+	m_maxKnobWidthPerGroup.reserve(m_carlaInstrument->m_paramGroupCount);
+	for (uint8_t i = 0; i < m_carlaInstrument->m_paramGroupCount; i++)
 	{
-		m_maxKnobWidthPerGroup.append(0);
+		m_maxKnobWidthPerGroup[i] = 0;
 	}
 
 	if (m_carlaInstrument->m_paramModels.empty()) { return; }
